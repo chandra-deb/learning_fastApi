0bf6a63a496858c32d7ed40fae5b81c4e4c720cd
chandra-deb <mail.chandradeb@gmail.com>
2023-01-15 00:04:01 +0600

    done CRUD using raw sql

--- app/main.py
+++ app/main.py
@@ -1,7 +1,49 @@
+import os
+import time
+from fastapi import FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
+import psycopg
+from psycopg.rows import dict_row
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv("host")
+db_name = os.getenv("dbname")
+db_user = os.getenv("user")
+db_password = os.getenv("password")
+
+
+while True:
+    try:
+        conn = psycopg.connect(
+            host=db_host,
+            dbname=db_name,
+            user=db_user,
+            password=db_password,
+            row_factory=dict_row,
+        )
+        cursor = conn.cursor()
+        print("Successfully connect")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print(error)
+        time.sleep(3)
+
+
+app = FastAPI()
+
+
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+
+
+@app.get("/")
+def root():
+    return {"name": "Nothing"}
 
 
 @app.get("/posts")
