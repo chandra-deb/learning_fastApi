26462156a02718f737d638414a397c15fecf9aa3
chandra-deb <mail.chandradeb@gmail.com>
2023-01-15 00:02:08 +0600

    implement get single post route

--- app/main.py
+++ app/main.py
@@ -15,3 +15,21 @@ def get_posts():
 
     return {"posts": posts}
 
+
+@app.get("/posts/{id:int}")
+def get_post(id: int):
+    cursor.execute(
+        """
+        SELECT * FROM posts WHERE id=%s
+    """,
+        (id,),
+    )
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {id} not found!",
+        )
+
+    return {"post": post}
+
