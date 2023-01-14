a49b7075a6f66ecbb72a68106d32eaa4f849b2f3
chandra-deb <mail.chandradeb@gmail.com>
2023-01-15 00:01:21 +0600

    implement get posts route

--- app/main.py
+++ app/main.py
@@ -3,3 +3,15 @@ class Post(BaseModel):
     content: str
     published: bool = True
 
+
+@app.get("/posts")
+def get_posts():
+    cursor.execute(
+        """
+        SELECT * FROM posts
+    """
+    )
+    posts = cursor.fetchall()
+
+    return {"posts": posts}
+
