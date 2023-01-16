75304f03807a3f78fae882e7d1bac1111c074e57
chandra-deb <mail.chandradeb@gmail.com>
2023-01-16 21:27:59 +0600

    create user model

--- app/models.py
+++ app/models.py
@@ -13,3 +13,14 @@ class Post(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
